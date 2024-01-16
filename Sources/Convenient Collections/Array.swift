0a0a89d31cf72a46840283e68402e38234e9ff06
Roman Suvorov <kikiwora@icloud.com>
2024-01-16 12:05:45 +0200

    update `Array` & `OrderedSet`

--- Sources/Convenient Collections/Array.swift
+++ Sources/Convenient Collections/Array.swift
@@ -1,32 +1,71 @@
 //  Created by Roman Suvorov (kikiwora)
 
-public extension Optional where Wrapped: Collection {
-    var isEmptyOrNil: Bool { self?.isEmpty ?? true }
-}
+import Foundation
 
 public extension Array {
-    static func compact<T>(_ array: Array<T?>) -> Array<T> {
-        array.compactMap { $0 }
+  static func compact<T>(_ array: [T?]) -> [T] {
+    array.compactMap { $0 }
+  }
+
+  func compact<T>() -> [T] where Element == T? {
+    .compact(self)
+  }
+
+  func find(_ condition: (Element) -> Bool) -> Element? {
+    for element in self {
+      if condition(element) {
+        return element
+      }
+    }
+    return nil
+  }
+
+  func lift(_ index: Int) -> Element? {
+    if index < 0 || index >= count {
+      return nil
+    }
+    return self[index]
+  }
+
+  mutating func removeFirst(where shouldRemove: (Element) -> Bool) {
+    for (index, item) in enumerated() {
+      if shouldRemove(item) {
+        remove(at: index)
+        return
+      }
+    }
+  }
+
+  func lazyFirst<T>() -> T? where Element == (() -> T?) {
+    for closure in self {
+      guard let result = closure() else { continue }
+      return result
     }
 
-    func compact<T>() -> [T] where Element == T? {
-        .compact(self)
-    }
+    return nil
+  }
 
-    func lazyFirst<T>() -> T? where Element == (()->T?) {
-        for closure in self {
-            guard let result = closure() else { continue }
-            return result
-        }
-
-        return nil
-    }
+  func tail(startingAt index: Int) -> Self {
+    Array(suffix(from: index))
+  }
 }
 
 public extension Array where Element: Equatable {
-    mutating func remove(_ element: Element) {
-        if let index = firstIndex(of: element) {
-            remove(at: index)
-        }
+  /// Works correctly only for arrays with unique elements
+  func next(after element: Element) -> Element? {
+    guard let index = firstIndex(of: element) else { return nil }
+    return lift(index + 1)
+  }
+
+  /// Works correctly only for arrays with unique elements
+  func previous(before element: Element) -> Element? {
+    guard let index = firstIndex(of: element) else { return nil }
+    return lift(index - 1)
+  }
+
+  mutating func remove(_ element: Element) {
+    if let index = firstIndex(of: element) {
+      remove(at: index)
     }
+  }
 }
--- Sources/Convenient Collections/OrderedSet.swift
+++ Sources/Convenient Collections/OrderedSet.swift
@@ -5,131 +5,131 @@
 /// An ordered set is an ordered collection of instances of `Element` in which
 /// uniqueness of the objects is guaranteed.
 public struct OrderedSet<E: Hashable>: Equatable, Collection {
-    public typealias Element = E
-    public typealias Index = Int
+  public typealias Element = E
+  public typealias Index = Int
 
-#if swift(>=4.1.50)
-    public typealias Indices = Range<Int>
-#else
-    public typealias Indices = CountableRange<Int>
-#endif
+  #if swift(>=4.1.50)
+  public typealias Indices = Range<Int>
+  #else
+  public typealias Indices = CountableRange<Int>
+  #endif
 
-    private var array: [Element]
-    private var set: Set<Element>
+  private var array: [Element]
+  private var set: Set<Element>
 
-    /// Creates an empty ordered set.
-    public init() {
-        array = []
-        set = Set()
+  /// Creates an empty ordered set.
+  public init() {
+    array = []
+    set = Set()
+  }
+
+  /// Creates an ordered set with the contents of `array`.
+  ///
+  /// If an element occurs more than once in `element`, only the first one
+  /// will be included.
+  public init(_ array: [Element]) {
+    self.init()
+    for element in array {
+      append(element)
     }
+  }
 
-    /// Creates an ordered set with the contents of `array`.
-    ///
-    /// If an element occurs more than once in `element`, only the first one
-    /// will be included.
-    public init(_ array: [Element]) {
-        self.init()
-        for element in array {
-            append(element)
-        }
+  // MARK: - Interfaces
+
+  /// The number of elements the ordered set stores.
+  public var count: Int { array.count }
+
+  /// Returns `true` if the set is empty.
+  public var isEmpty: Bool { array.isEmpty }
+
+  /// Returns the contents of the set as an Array.
+  public var contents: [Element] { array }
+
+  /// Returns the contents of the set as a Set.
+  public var unique: Set<Element> { self.set }
+
+  /// Returns `true` if the ordered set contains `member`.
+  public func contains(_ member: Element) -> Bool {
+    set.contains(member)
+  }
+
+  /// Adds an element to the ordered set.
+  ///
+  /// If it already contains the element, then the set is unchanged.
+  ///
+  /// - returns: True if the item was inserted.
+  @discardableResult
+  public mutating func append(_ newElement: Element) -> Bool {
+    let inserted = set.insert(newElement).inserted
+    if inserted {
+      array.append(newElement)
     }
+    return inserted
+  }
 
-    // MARK: - Interfaces
+  /// Remove and return the element at the beginning of the ordered set.
+  @discardableResult
+  public mutating func removeFirst() -> Element {
+    let firstElement = array.removeFirst()
+    set.remove(firstElement)
+    return firstElement
+  }
 
-    /// The number of elements the ordered set stores.
-    public var count: Int { array.count }
+  @discardableResult
+  public mutating func removeFirst(_ batchSize: Int) -> [Element] {
+    guard batchSize > .zero else { return .empty }
 
-    /// Returns `true` if the set is empty.
-    public var isEmpty: Bool { array.isEmpty }
+    let batchToRemove = Array(array.prefix(batchSize))
+    array.removeFirst(batchSize)
+    set.subtract(Set(batchToRemove))
+    return batchToRemove
+  }
 
-    /// Returns the contents of the set as an Array.
-    public var contents: [Element] { array }
+  /// Remove and return the element at the end of the ordered set.
+  @discardableResult
+  public mutating func removeLast() -> Element {
+    let lastElement = array.removeLast()
+    set.remove(lastElement)
+    return lastElement
+  }
 
-    /// Returns the contents of the set as a Set.
-    public var unique: Set<Element> { self.set }
+  /// Remove all elements.
+  public mutating func removeAll(keepingCapacity keepCapacity: Bool = false) {
+    array.removeAll(keepingCapacity: keepCapacity)
+    set.removeAll(keepingCapacity: keepCapacity)
+  }
 
-    /// Returns `true` if the ordered set contains `member`.
-    public func contains(_ member: Element) -> Bool {
-        set.contains(member)
-    }
-
-    /// Adds an element to the ordered set.
-    ///
-    /// If it already contains the element, then the set is unchanged.
-    ///
-    /// - returns: True if the item was inserted.
-    @discardableResult
-    public mutating func append(_ newElement: Element) -> Bool {
-        let inserted = set.insert(newElement).inserted
-        if inserted {
-            array.append(newElement)
-        }
-        return inserted
-    }
-
-    /// Remove and return the element at the beginning of the ordered set.
-    @discardableResult
-    public mutating func removeFirst() -> Element {
-        let firstElement = array.removeFirst()
-        set.remove(firstElement)
-        return firstElement
-    }
-
-    @discardableResult
-    public mutating func removeFirst(_ batchSize: Int) -> [Element] {
-        guard batchSize > .zero else { return [] }
-
-        let batchToRemove = Array(array.prefix(batchSize))
-        array.removeFirst(batchSize)
-        set.subtract(Set(batchToRemove))
-        return batchToRemove
-    }
-
-    /// Remove and return the element at the end of the ordered set.
-    @discardableResult
-    public mutating func removeLast() -> Element {
-        let lastElement = array.removeLast()
-        set.remove(lastElement)
-        return lastElement
-    }
-
-    /// Remove all elements.
-    public mutating func removeAll(keepingCapacity keepCapacity: Bool = false) {
-        array.removeAll(keepingCapacity: keepCapacity)
-        set.removeAll(keepingCapacity: keepCapacity)
-    }
-
-    /// Inserts new unique contents of `Array` into the `OrderedSet`
-    /// - Parameter array: `Array` of contents to insert
-    public mutating func insert(_ array: [Element]) {
-        array.forEach { self.append($0) }
-    }
+  /// Inserts new unique contents of `Array` into the `OrderedSet`
+  /// - Parameter array: `Array` of contents to insert
+  public mutating func insert(_ array: [Element]) {
+    array.forEach { append($0) }
+  }
 }
 
 // MARK: - ExpressibleByArrayLiteral
 
 extension OrderedSet: ExpressibleByArrayLiteral {
-    /// Create an instance initialized with `elements`.
-    ///
-    /// If an element occurs more than once in `element`, only the first one
-    /// will be included.
-    public init(arrayLiteral elements: Element...) {
-        self.init(elements)
-    }
+  /// Create an instance initialized with `elements`.
+  ///
+  /// If an element occurs more than once in `element`, only the first one
+  /// will be included.
+  public init(arrayLiteral elements: Element...) {
+    self.init(elements)
+  }
 }
 
 // MARK: - RandomAccessCollection
 
 extension OrderedSet: RandomAccessCollection {
-    public var startIndex: Int { contents.startIndex }
-    public var endIndex: Int { contents.endIndex }
-    public subscript(index: Int) -> Element {
-        contents[index]
-    }
+  public var startIndex: Int { contents.startIndex }
+  public var endIndex: Int { contents.endIndex }
+  public subscript(index: Int) -> Element {
+    contents[index]
+  }
 }
 
 public func == <T>(lhs: OrderedSet<T>, rhs: OrderedSet<T>) -> Bool {
-    lhs.contents == rhs.contents
+  lhs.contents == rhs.contents
 }
 
 // MARK: - Hashable
@@ -137,27 +137,27 @@ public func == <T>(lhs: OrderedSet<T>, rhs: OrderedSet<T>) -> Bool {
 extension OrderedSet: Hashable where Element: Hashable {}
 
 public extension OrderedSet {
-    mutating func remove(_ element: Element) {
-        array.remove(element)
-        set.remove(element)
-    }
+  mutating func remove(_ element: Element) {
+    array.remove(element)
+    set.remove(element)
+  }
 }
 
 // MARK: - Encodable
 
 extension OrderedSet: Encodable where E: Encodable {
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(contents)
-    }
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(contents)
+  }
 }
 
 // MARK: - Decodable
 
 extension OrderedSet: Decodable where E: Decodable {
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let array = try container.decode([E].self)
-        self.init(array)
-    }
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let array = try container.decode([E].self)
+    self.init(array)
+  }
 }
